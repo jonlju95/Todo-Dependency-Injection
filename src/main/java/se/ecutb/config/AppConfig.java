b98c6395d87d8cccca0e5c4c43b19586adac0028
crazvik <crazvik@gmail.com>
2020-02-05 09:50:37 +0100

    PersonRepositoryTest

--- src/main/java/se/ecutb/config/AppConfig.java
+++ src/main/java/se/ecutb/config/AppConfig.java
@@ -1,9 +1,23 @@
 package se.ecutb.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import se.ecutb.data.PersonRepository;
+import se.ecutb.data.PersonRepositoryImpl;
+import se.ecutb.data.TodoRepository;
+import se.ecutb.data.TodoRepositoryImpl;
 
 @Configuration
-@ComponentScan("se.ecutb")
+@ComponentScan("se.ecutb.jonatanl")
 public class AppConfig {
+    @Bean
+    public PersonRepository personRepository() {
+        return new PersonRepositoryImpl();
+    }
+
+    @Bean
+    public TodoRepository todoRepository() {
+        return new TodoRepositoryImpl();
+    }
 }
---
+++ src/main/java/se/ecutb/data/PersonRepositoryImpl.java
@@ -0,0 +1,106 @@
+package se.ecutb.data;
+
+import org.springframework.stereotype.Service;
+import se.ecutb.model.Address;
+import se.ecutb.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class PersonRepositoryImpl implements PersonRepository {
+    private List<Person> group = new ArrayList<>();
+
+    @Override
+    public Optional<Person> findById(int personId) {
+        return group.stream().filter(person -> person.getPersonId()==personId)
+                .findFirst();
+    }
+
+    @Override
+    public Person persist(Person person) throws IllegalArgumentException {
+        for (int i=0; i<group.size(); i++) {
+            if (group.get(i).getEmail().equalsIgnoreCase(person.getEmail())) {
+                throw new IllegalArgumentException();
+            }
+        }
+        group.add(person);
+        return person;
+    }
+
+    @Override
+    public Optional<Person> findByEmail(String email) {
+        return group.stream().filter(person -> person.getEmail().equalsIgnoreCase(email))
+                .findFirst();
+    }
+
+    @Override
+    public List<Person> findByAddress(Address address) {
+        List<Person> wantedAddress = new ArrayList<>();
+        for (Person person: group) {
+            if (Objects.equals(person.getAddress(), address)) {
+                wantedAddress.add(person);
+            }
+        }
+        return wantedAddress;
+    }
+
+    @Override
+    public List<Person> findByCity(String city) {
+        List<Person> wantedCity = new ArrayList<>();
+        for (Person person: group) {
+            if (Objects.isNull(person.getAddress())) {
+                continue;
+            }
+            if (Objects.equals(person.getAddress().getCity(), city)) {
+                wantedCity.add(person);
+            }
+        }
+        return wantedCity;
+    }
+
+    @Override
+    public List<Person> findByLastName(String lastName) {
+        return group.stream().filter(person -> person.getLastName().equalsIgnoreCase(lastName))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Person> findByFullName(String fullName) {
+        List<Person> wantedName = new ArrayList<>();
+        for (Person person: group) {
+            String name = person.getFirstName() + " " + person.getLastName();
+            if (name.equalsIgnoreCase(fullName)) {
+                wantedName.add(person);
+            }
+        }
+        return wantedName;
+    }
+
+    @Override
+    public List<Person> findAll() {
+        return group;
+    }
+
+    @Override
+    public boolean delete(int personId) throws IllegalArgumentException {
+        for (Person person: group) {
+            if(person.getPersonId()==personId) {
+                group.remove(person);
+                return true;
+            }
+            else {
+                throw new IllegalArgumentException();
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        group.clear();
+    }
+}
